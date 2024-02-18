0070c548e838c7753bb24dc9df12f1575c7f9e53
Dinesh Kumar <61492032+choudhary-dinesh@users.noreply.github.com>
2024-02-18 17:03:42 +0530

    Update config.py for task 1 dataset path

--- Assignment1/config.py
+++ Assignment1/config.py
@@ -1,4 +1,7 @@
 ## Sentiment classification raw data
-data_path = "/content/drive/MyDrive/Natural Language Understanding/Assignment1/data/AMAZON_FASHION.csv"
+data_path = "/content/AMAZON_FASHION.csv"
 vectorizer = "delta_tf_idf"
+ner_pos_data_path = '/content/NER_Dataset.csv'
+
+
 

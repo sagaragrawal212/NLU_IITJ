e0206594ef1306ecb73f1d5159ae703510b627b0
Dinesh Kumar <61492032+choudhary-dinesh@users.noreply.github.com>
2024-02-18 17:05:39 +0530

    added preprocessing pipeline for task 2

--- Assignment1/pre_process.py
+++ Assignment1/pre_process.py
@@ -1,5 +1,16 @@
 import string
 import ast
+#imports for preprocessing task 2
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+lemmatizer = WordNetLemmatizer()
+# import string
+import re
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('wordnet')
 
 def lowercase_text(text):
     # Convert the text to lowercase
@@ -7,7 +18,7 @@ def lowercase_text(text):
 
 def remove_punctuation(text):
     # Remove punctuation from the text
-    punctuations = """!"#$%&'()*+,-/:;<=>?[\]^_`{|}~'""" #except . @
+    punctuations = """!"#%&'()*+,-/:;<=>?[\]^_`{|}~'""" #except . @ $
     translator = str.maketrans('', '', string.punctuation)
     return text.translate(translator)
 
@@ -49,3 +60,41 @@ def preprocess_pos_ner(row) :
       return None , None , None
     else :
       return new_word,new_pos,new_ner
+
+def preprocess_text_task2(text):
+    # Lowercasing
+    text = text.lower()
+
+    # Removing URLs
+    text = re.sub(r'http\S+|www\S+', '', text)
+
+    # Removing emojis
+    emoji_pattern = re.compile("["
+                               u"\U0001F600-\U0001F64F"  # emoticons
+                               u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                               u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                               u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                               "]+", flags=re.UNICODE)
+    text = emoji_pattern.sub(r'', text)
+
+    # Removing special characters, keeping letters numbers,
+    # text = re.sub(r'[^a-zA-Z\s]', '', text)
+    text = re.sub(r'[^\w\s]|_', '', text)
+
+    # Removing punctuation
+    text = text.translate(str.maketrans('', '', string.punctuation))
+
+    # Tokenization
+    tokens = word_tokenize(text)
+
+    # Removing stopwords
+    stop_words = set(stopwords.words('english'))
+    filtered_tokens = [word for word in tokens if word not in stop_words]
+
+    # Lemmatization
+    lemmatized_tokens = [lemmatizer.lemmatize(word) for word in filtered_tokens]
+
+    # Joining tokens back into a single string
+    preprocessed_text = ' '.join(lemmatized_tokens)
+
+    return preprocessed_text
